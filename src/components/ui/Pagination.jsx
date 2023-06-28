ceb54a259ba531e01fd440d517233bbf747bed79
Vjeko <dizdar.vjeko@gmail.com>
2023-06-28 16:31:35 +0200

    changed bg and color of pagination numbers when active

--- src/components/ui/Pagination.jsx
+++ src/components/ui/Pagination.jsx
@@ -24,16 +24,23 @@ const StyledPagination = styled.div`
         visibility: hidden;
         opacity: 0;
       }
+
+      a {
+        padding: 0.313rem 0.438rem;
+        border-radius: 0.313rem;
+        color: ${({ theme }) => theme.colors.blue};
+      }
     }
 
     .page-link {
-      padding: 0.313rem 0.438rem;
       ${mixins.flex('center', 'center')};
-      border-radius: 0.313rem;
-    }
 
-    .active {
-      background: rgba(179, 179, 179, 0.2);
+      &.active {
+        a {
+          background: ${({ theme }) => theme.colors.blue};
+          color: ${({ theme }) => theme.colors.white};
+        }
+      }
     }
   }
 `;
